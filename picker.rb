a34060f7b5de8c31403c622368652255d2bef6ab
Karntchana Sangjan <karntchanas.mo5214@gmail.com>
2014-10-30 09:45:05 +0700

    feature c commit

--- picker.rb
+++ picker.rb
@@ -6,6 +6,19 @@ class Engineer
    def pick_engineer
       @engineers.sample #random and return string
    end
+   
+   def featureC(txt)
+      x = @engineer.find_all { |line| line =~ /#{Regexp.quote(txt)}/ } # the magic by looking up names containing string
+      x.each{|a|
+        nameList.push(a)
+        }
+        puts nameList[rand(nameList.length)] ## fetch random name and print it.
+      end
+   end
+   
+   
 end
 test = Engineer.new()
 puts test.pick_engineer
+puts "testing Feature C"
+puts test.featureC('arn')
