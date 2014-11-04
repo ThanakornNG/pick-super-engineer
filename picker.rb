9ed35328f0914c4b0cdda8ee7d15f5cd88e82481
Karntchana Sangjan <karntchanas.mo5214@gmail.com>
2014-11-04 20:55:48 +0700

    glitch fix III :)

--- picker.rb
+++ picker.rb
@@ -21,7 +21,9 @@ class Engineer
       x.each{|a|
         nameList.push(a)#
       }
-      puts nameList[rand(nameList.length)] ## fetch random name from searched list and print it.
+      x = nameList[rand(nameList.length)] ## fetch random name from searched list and print it.
+      #puts "feature C randomed : ",x
+      return x
       
    end
 end
@@ -29,6 +31,7 @@ end
 
 test = Engineer.new()
 
+#debug derby!
 =begin
 puts name
 puts "testing Feature A"#debug
@@ -38,31 +41,32 @@ puts test.featureB
 puts "testing Feature C"#debug
 =end 
 
+=begin
+	puts "name is " , name
+	puts "type is",name.class
+	puts "t is" , t
+	puts "type is",t.class
+=end
+
 if ! ARGV.empty?# check if argument is nil
-	name = test.featureC(ARGV[0]) # grab the argument
-	
-	if name != ""
+  t = ARGV[0]
+	name = test.featureC(t) # grab the argument
+end
+
+
+if name == nil
+  name = test.pick_engineer# grab the argument
+  #name = "admiral ackbar"
+end
+
+if name != ""
 		puts "1. %s" % [name]# print out randomed name by search argument
 
 		puts "2. %s" % [test.pick_featureA(name)]
 
 		puts "3. %s" % [test.featureB(name)]
-	end
 end
 
-if ARGV.empty?# check if argument empty
-	
-	
-	if name != ""
-	  name = test.pick_engineer# grab the argument
-	  #name = "arack akbar"# #debug 
-		puts "1. %s" % [name]# print out randomed name by search argument
-
-		puts "2. %s" % [test.pick_featureA(name)]
-
-		puts "3. %s" % [test.featureB(name)]
-	end
-end
 
 
 
